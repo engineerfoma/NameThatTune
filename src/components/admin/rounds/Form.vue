a7d97dc68349d66b66158adba6e8ce61a7bebca4
Nikolay Fomenko <fomen_94@bk.ru>
2024-09-03 00:45:12 +0300

    feat: add form component for melody page

--- src/components/admin/rounds/Form.vue
+++ src/components/admin/rounds/Form.vue
@@ -1,5 +1,97 @@
 <template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row class="w-100 d-flex flex-row align-center">
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="data.name"
+            :counter="10"
+            :rules="nameRules"
+            label="Введите название команды"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
 
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-select
+            style="height: 56px"
+            clearable
+            label="Выберите цвет команды"
+            v-model="data.color"
+            required
+            :items="[
+              'Красный',
+              'Синий',
+              'Зеленый',
+              'Бирюзовый',
+              'Фиолетовый',
+              'Оранжевый',
+            ]"
+          ></v-select>
+        </v-col>
+        <v-tooltip
+          v-if="!data.completed"
+          text="Создаить команду"
+          location="end"
+        >
+          <template v-slot:activator="{ props }">
+            <v-icon
+              :height="30"
+              :width="30"
+              size="x-large"
+              class="v-img flex-0-0"
+              v-bind="props"
+              @click="data.completed = true"
+              icon="mdi-check-circle"
+            ></v-icon>
+          </template>
+        </v-tooltip>
+        <v-tooltip
+          text="Удалить команду"
+          location="end"
+        >
+          <template v-slot:activator="{ props }">
+            <v-img
+              :height="30"
+              :width="30"
+              class="v-img flex-0-0"
+              v-bind="props"
+              src="@/assets/icons/delete.svg"
+              alt="delete"
+              @click="$emit('removeForm', index)"
+            />
+          </template>
+        </v-tooltip>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
-<script>
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    default: null,
+  },
+  index: Number,
+})
 </script>
+<style lang="scss" scoped>
+.v-img {
+  width: 50px;
+  transition: opacity 0.3s ease;
+  &__img {
+    width: 50px;
+  }
+  &:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+}
+</style>
--- src/components/admin/rounds/RoundOne.vue
+++ src/components/admin/rounds/RoundOne.vue
@@ -11,22 +11,50 @@
       </v-btn>
       <v-btn
         append-icon="mdi-plus"
-        @click="addTeam"
+        @click="addForm"
+        :disabled="forms.length >= 6"
         >Добавить команду</v-btn
       >
+      <!-- <v-btn
+        append-icon="mdi-plus"
+        @click="addTeam"
+        >Добавить команду api</v-btn
+      > -->
       <v-btn
         append-icon="mdi-check-circle"
         @click="getTeams"
         >получить список команд</v-btn
       >
     </div>
-    <div>
-
+    <div
+      class="pt-6 d-flex ga-sm-6 flex-row w-100 flex-column"
+      v-if="forms.length"
+    >
+      <Form
+        v-for="(form, index) in forms"
+        :key="form.id"
+        :data="form"
+        :index="index"
+        @removeForm="removeFormHandler"
+      />
+      {{ forms }}
     </div>
   </div>
 </template>
 <script setup>
 import { team } from '@/services/api.service'
+
+const forms = ref([])
+
+const addForm = () => {
+  forms.value.push({
+    id: Math.floor(Math.random() * 100),
+    name: '',
+    color: '',
+    points: 0,
+    complete: false,
+  })
+}
 
 const removeAllTeams = async () => {
   const data = await team.removeAll()
@@ -45,5 +73,11 @@ const addTeam = async () => {
 const getTeams = async () => {
   const data = await team.get()
   console.log(data)
+}
+
+const removeFormHandler = (index) => {
+  console.log('index ', index)
+
+  forms.value.splice(index, 1)
 }
 </script>
